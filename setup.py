7fbb80ba74e1bc275d394fb51d544b751d7c9ca3
lorenzo-osimani <lorenzo.osimani@studio.unibo.it>
2023-06-29 23:11:43 +0200

    Versioning pypi

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ def get_version_from_git():
         if version_file.exists():
             return version_file.read_text().strip()
         else:
-            return '0.1.0'
+            return '0.2.0'
 
 
 version = get_version_from_git()
