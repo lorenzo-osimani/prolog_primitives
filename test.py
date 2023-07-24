13baffd29aeb5eb455c6267c15f0afc1187a0a3c
lorenzo-osimani <lorenzo.osimani@studio.unibo.it>
2023-07-24 14:37:08 +0200

    Modified dataset test, added timer for performance

--- test.py
+++ test.py
@@ -1,38 +1,54 @@
 import tensorflow as tf
+import pandas as pd
+import numpy as np
 from tensorflow import keras
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
-# Input data files are available in the "../input/" directory.
-# For example, running this (by clicking run or pressing Shift+Enter) will list the files in the input directory
-(train_features, train_labels), (test_features, test_labels) = keras.datasets.boston_housing.load_data()
+from sklearn.model_selection import KFold
+import time
 
+start_time = time.time()
+url = 'http://archive.ics.uci.edu/ml/machine-learning-databases/auto-mpg/auto-mpg.data'
+column_names = ['MPG', 'Cylinders', 'Displacement', 'Horsepower', 'Weight',
+                'Acceleration', 'Model Year', 'Origin']
+
+dataset = pd.read_csv(url, names=column_names,
+                          na_values='?', comment='\t',
+                          sep=' ', skipinitialspace=True).copy().dropna()
+
+X = dataset.drop('MPG', axis = 1)
+y = dataset.pop('MPG')
 
 normalizer = tf.keras.layers.Normalization(axis = 1)
-normalizer.adapt(train_features)
-train_features = normalizer(train_features)
-test_features = normalizer(test_features)
+normalizer.adapt(X)
+X = normalizer(X)
 
-print(train_features)
+def createModel():
+    model = keras.Sequential([
+        tf.keras.layers.Dense(128, input_shape=(7, ), activation='relu', name='dense_1'),
+        tf.keras.layers.Dense(64, activation='relu', name='dense_2'),
+        tf.keras.layers.Dense(1, activation='linear', name='dense_output'),
+    ])
+    model.compile(
+                    loss = "mse",
+                    optimizer='adam',
+                    metrics=['mae'],
+                )
+    return model
 
+kfold = KFold(n_splits=5, shuffle=True, random_state=42)
 
-model = keras.Sequential([
-    tf.keras.layers.Dense(128, input_shape=(13, ), activation='relu', name='dense_1'),
-    tf.keras.layers.Dense(64, activation='relu', name='dense_2'),
-    tf.keras.layers.Dense(1, activation='linear', name='dense_output'),
-])
+scores = []
+for train_index, test_index in kfold.split(X):
+    X_train, X_test = np.take(X, train_index, axis = 0), np.take(X, test_index, axis = 0)
+    y_train, y_test = np.take(y, train_index, axis = 0), np.take(y, test_index, axis = 0)
+    
+    model = createModel()
+    model.fit(x=X_train,y=y_train, epochs=100, validation_split=0.05)
 
-model.compile(
-                loss = "mse",
-                optimizer='adam',
-                metrics=['mae'],
-            )
-model.fit(x=train_features,y=train_labels, epochs=100, validation_split=0.05)
+    predictions = model.predict(X_test)
 
-predictions = model.predict(test_features)
-print(predictions)
+    m = tf.keras.metrics.MeanAbsoluteError()
+    m.update_state(y_test, predictions)
+    scores.append(m.result().numpy())
+print(f'Accuracies: {scores}')
 
-m = tf.keras.metrics.MeanAbsoluteError()
-m.update_state(test_labels, predictions)
-score = m.result().numpy()
-
-print(score)
+print("--- %s seconds ---" % (time.time() - start_time))
